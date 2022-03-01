38df1f718e507de7763f304233686b5fcdbe2ddd
Luke Kirwan <40961351+thelk22@users.noreply.github.com>
2022-03-01 17:36:49 +0000

    modified upload button

--- polygon-hackathon/src/components/UploadFile.js
+++ polygon-hackathon/src/components/UploadFile.js
@@ -1,12 +1,23 @@
-import IconButton from "@mui/material/IconButton";
+import * as React from "react";
+import { styled } from "@mui/material/styles";
 import FileUploadIcon from "@mui/icons-material/FileUpload";
+import Button from "@mui/material/Button";
+
+const Input = styled("input")({
+  display: "none",
+});
 
 // This component contains an upload button, and a transfer button
 function UploadFile() {
+  const handleClick = (event) => {};
+
   return (
-    <IconButton aria-label="upload">
-      <FileUploadIcon />
-    </IconButton>
+    <label htmlFor="contained-button-file">
+      <Input accept="image/*" id="contained-button-file" multiple type="file" />
+      <Button variant="contained" component="span" onClick={handleClick}>
+        <FileUploadIcon fontSize="large" />
+      </Button>
+    </label>
   );
 }
 
